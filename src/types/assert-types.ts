d305b4c6001b2ad7a79a40811f46b25c6aa3f3a4
Behzad Ali Mohammad Zad <behzad.am@gmail.com>
2025-12-04 02:05:38 +0330

    Remove index.ts file and update VSCode settings to include custom spell check words

--- src/types/assert-types.ts
+++ src/types/assert-types.ts
@@ -1,5 +1,9 @@
 import { type Assert, type Equal, type Not } from 'asserttt';
 
+/**
+ * Keep it as an example of the usage of the asserttt library.
+ */
+
 type Pair<X> = [X, X];
 type _ = [
   Assert<Equal<
